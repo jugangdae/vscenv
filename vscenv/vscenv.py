9a6272c563b5e7c399b7ba81fa918516ae11d4d4
jgd <jugangdae@gmail.com>
2024-11-27 14:30:56 +0900

    fix messages

--- vscenv/vscenv.py
+++ vscenv/vscenv.py
@@ -8,93 +8,82 @@ import os
 import shutil
 import argparse
 import configparser
+import subprocess
 
 version = '0.1.1'
 
-vscenv_run_cmd = 'code' # 'code' or 'code-insider' 
+vscenv_run_cmd = 'code' # 'code' or 'code-insider'
 vscenv_dir_path = os.path.join(os.path.expanduser('~'), '.vscenv')
-vscenv_conf_path = os.path.join(os.path.expanduser('~'), '.vscenvconfig')
+vscenv_conf_path = os.path.join(os.path.expanduser('~'), '.vscenvconf')
 
 def _list(args):
-    global vscenv_dir_path
     for i in os.listdir(vscenv_dir_path):
         print(i)
     return 0
 
 def _create(args):
-    global vscenv_dir_path
-
     codeDir = os.path.join(vscenv_dir_path, args.env)
     userdataDir = os.path.join(codeDir, 'userdata')
     extensionsDir = os.path.join(codeDir, 'extensions')
     
     if os.path.exists(codeDir):
-        print(f'ERROR: {args.env} is exist..')
+        print(f'ERROR: {args.env} already exists.')
         return -1
 
     try:
-        os.makedirs(codeDir)
         os.makedirs(userdataDir)
         os.makedirs(extensionsDir)
-        print('DONE : Successfully created..')
-    except OSError:
-        print('ERROR: Failed to create..')
+        print('DONE: Successfully created.')
+    except OSError as e:
+        print(f'ERROR: Creation failed - {e}')
         return -1
     
     return 0
     
 def _delete(args):
-    global vscenv_dir_path
-
     codeDir = os.path.join(vscenv_dir_path, args.env)
 
     if not os.path.exists(codeDir):
-        print(f'ERROR: {args.env} is not found..')
+        print(f'ERROR: {args.env} not found.')
         return -1
 
     try:
         shutil.rmtree(codeDir)
-        print('DONE : Successfully deleted..')
-    except OSError:
-        print('ERROR: Failed to delete..')
+        print('DONE: Successfully deleted.')
+    except OSError as e:
+        print(f'ERROR: Deletion failed - {e}')
         return -1
     
     return 0
 
 def _run(args):
-    global vscenv_dir_path
-    global vscenv_run_cmd
-    
     codeDir = os.path.join(vscenv_dir_path, args.env)
     userdataDir = os.path.join(codeDir, 'userdata')
     extensionsDir = os.path.join(codeDir, 'extensions')
     
-    cmd = vscenv_run_cmd
+    cmd = [vscenv_run_cmd]
 
     if not os.path.exists(codeDir):
-        print(f'ERROR: {args.env} is not found..')
+        print(f'ERROR: {args.env} not found.')
         return -1
 
     if os.path.exists(userdataDir):
-        cmd =f'{cmd} --user-data-dir {userdataDir}'
+        cmd.extend(['--user-data-dir', userdataDir])
 
     if os.path.exists(extensionsDir):
-        cmd =f'{cmd} --extensions-dir {extensionsDir}'    
+        cmd.extend(['--extensions-dir', extensionsDir])    
     
+    cmd.append(args.path)
+
     try:
-        cmd = f'{cmd} {args.path}'
-        os.system(cmd)
-    except OSError:
-        print(f'ERROR: Faild to run {args.env}.')
+        subprocess.run(cmd)
+    except subprocess.CalledProcessError as e:
+        print(f'ERROR: Failed to run {args.env} - {e}')
         return -1
 
     return 0
 
 def main():
-    global vscenv_conf_path
-    global vscenv_dir_path
-    global vscenv_run_cmd
-    
     if not os.path.exists(vscenv_conf_path):
         config_parser = configparser.ConfigParser()
         config_parser.add_section("setting")
@@ -107,8 +96,6 @@ def main():
         config_parser.read(vscenv_conf_path)
         vscenv_run_cmd = config_parser['setting']['vscenv_run']
         vscenv_dir_path = config_parser['setting']['vscenv_dir']
-        # print(f'run_cmd = {vscenv_run_cmd}')
-        # print(f'dir_path = {vscenv_dir_path}')
   
     if not os.path.exists(vscenv_dir_path):
         os.makedirs(vscenv_dir_path)
@@ -119,15 +106,15 @@ def main():
     subparsers = arg_parser.add_subparsers()
     cmd_list = subparsers.add_parser('list', aliases=['l'], help='Show list of vscenv environments.')
     cmd_list.set_defaults(func=_list)
-    cmd_create = subparsers.add_parser('create', aliases=['c'], help='Create a new vscenv environmtent.')
-    cmd_create.add_argument('env')
+    cmd_create = subparsers.add_parser('create', aliases=['c'], help='Create a new vscenv environment.')
+    cmd_create.add_argument('env', help='Name of the environment to create')
     cmd_create.set_defaults(func=_create)
-    cmd_delete = subparsers.add_parser('delete', aliases=['d'], help='Delete an vscenv environmtent.')
-    cmd_delete.add_argument('env')
+    cmd_delete = subparsers.add_parser('delete', aliases=['d'], help='Delete a vscenv environment.')
+    cmd_delete.add_argument('env', help='Name of the environment to delete')
     cmd_delete.set_defaults(func=_delete)
-    cmd_run = subparsers.add_parser('run', aliases=['r'], help='Executes VSCODE using an vscenv environment.')
-    cmd_run.add_argument('env')
-    cmd_run.add_argument('path')
+    cmd_run = subparsers.add_parser('run', aliases=['r'], help='Run VSCODE using a vscenv environment.')
+    cmd_run.add_argument('env', help='Name of the environment to run')
+    cmd_run.add_argument('path', help='Path to the file to open')
     cmd_run.set_defaults(func=_run)
     
     args = arg_parser.parse_args()
